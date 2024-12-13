84effbce6f55e49e5b96f3f90ee7721b5b949f70
turbokuzmich <kurteev.d@yandex.ru>
2024-12-13 11:40:54 +0300

    add vk pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,17 @@ export default function RootLayout({
     webvisor:true
   });`}
       </Script>
+      <Script>
+        {`var _tmr = window._tmr || (window._tmr = []);
+_tmr.push({id: "3589962", type: "pageView", start: (new Date()).getTime()});
+(function (d, w, id) {
+  if (d.getElementById(id)) return;
+  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+  ts.src = "https://top-fwz1.mail.ru/js/code.js";
+  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+})(document, window, "tmr-code");`}
+      </Script>
       <body>
         <GoogleAnalytics gaId="G-6JC9JR7TQ0" />
         <AppRouterCacheProvider>
